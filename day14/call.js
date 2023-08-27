ff5375ea4c6480b8d22b2a0b4d65287a1c21dfc6
Rahmatullah1983 <bugtirahmatullah@gmail.com>
2023-08-27 13:02:13 +0500

    tody I have learnt call bind and apply methods

---
+++ day14/call.js
@@ -0,0 +1,35 @@
+/*const person = {
+    firstName: "Rahmatullah",
+    lastName: " Bugti",
+    getName(){
+        console.log(this.firstName +this.lastName);
+    }
+};
+person.getName();
+
+function regiterUser(country, lang){
+    console.log(`My country is ${country} and my language is ${lang} `);
+}
+//bind
+let register =regiterUser.bind(person);
+register("Pakistan", " Urdu");
+*/
+const user={
+
+}
+
+function reg(name, caste){
+    console.log(`My name is ${name} and my caste is ${caste} `);
+}
+//call
+reg.call(user, "Rahmatullah", "Bugti");
+
+const user2={
+
+}
+
+function regt(desgnation, qualification){
+    console.log(`My desgnation is ${desgnation} and my qualificaion is ${qualification} `);
+}
+//apply
+regt.apply(user2, ["Junior Devloper", "Software Engineer"]);
